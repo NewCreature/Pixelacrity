6a0c6dffe6065c19e0015a4b5a4df49c458e0489
Todd Cope <todd@t3-i.com>
2021-01-27 11:33:52 -0600

    Forgot to add files for pixel tool.

---
+++ src/ui/tool_pixel.c
@@ -0,0 +1,24 @@
+#include "t3f/t3f.h"
+#include "canvas.h"
+#include "canvas_editor.h"
+
+static void draw_pixel(QUIXEL_CANVAS_EDITOR * cep, QUIXEL_CANVAS * cp, int x, int y, ALLEGRO_COLOR color)
+{
+	ALLEGRO_TRANSFORM identity;
+	ALLEGRO_STATE old_state;
+
+	al_store_state(&old_state, ALLEGRO_STATE_TARGET_BITMAP | ALLEGRO_STATE_TRANSFORM);
+	al_set_target_bitmap(cp->layer[cep->current_layer]->bitmap[y / cp->bitmap_size][x / cp->bitmap_size]);
+	al_identity_transform(&identity);
+	al_draw_pixel(x % cp->bitmap_size, y % cp->bitmap_size, color);
+	al_restore_state(&old_state);
+}
+
+void quixel_tool_pixel_logic(QUIXEL_CANVAS_EDITOR * cep, QUIXEL_CANVAS * cp)
+{
+	if(!quixel_expand_canvas(cp, cep->current_layer, cep->view_x, cep->view_y))
+	{
+		printf("Tried to expand canvas but failed!\n");
+	}
+	draw_pixel(cep, cp, cep->hover_x, cep->hover_y, al_map_rgba_f(cep->current_layer == 0 ? 1.0 : 0.0, cep->current_layer == 1 ? 1.0 : 0.0, cep->current_layer == 2 ? 1.0 : 0.0, 1.0));
+}
---
+++ src/ui/tool_pixel.h
@@ -0,0 +1,6 @@
+#ifndef QUIXEL_TOOL_PIXEL_H
+#define QUIXEL_TOOL_PIXEL_H
+
+void quixel_tool_pixel_logic(QUIXEL_CANVAS_EDITOR * cep, QUIXEL_CANVAS * cp);
+
+#endif
