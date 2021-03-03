e3a90809f465fe084652f1dcb9e06b33348bba65
Todd Cope <todd@t3-i.com>
2021-03-03 13:27:54 -0600

    Properly free memory on failure.

--- src/pixel_shader.c
+++ src/pixel_shader.c
@@ -2,27 +2,37 @@
 
 ALLEGRO_SHADER * quixel_create_pixel_shader(const char * fn)
 {
-	ALLEGRO_SHADER * shader;
+	ALLEGRO_SHADER * shader = NULL;
 
 	shader = al_create_shader(ALLEGRO_SHADER_AUTO);
 	if(!shader)
 	{
-		return NULL;
+		goto fail;
 	}
 
 	if(!al_attach_shader_source(shader, ALLEGRO_VERTEX_SHADER, al_get_default_shader_source(ALLEGRO_SHADER_AUTO, ALLEGRO_VERTEX_SHADER)))
 	{
-		return NULL;
+		goto fail;
 	}
 
 	if(!al_attach_shader_source_file(shader, ALLEGRO_PIXEL_SHADER, fn))
 	{
-		return NULL;
+		goto fail;
 	}
 	if(!al_build_shader(shader))
 	{
-		return NULL;
+		goto fail;
 	}
 
 	return shader;
+
+	fail:
+	{
+		if(shader)
+		{
+			printf("Shader error:\n\n%s\n", al_get_shader_log(shader));
+			al_destroy_shader(shader);
+		}
+		return NULL;
+	}
 }
