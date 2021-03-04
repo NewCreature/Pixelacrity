732d5a87c2c510b89e51940f624f5c963aab4288
Todd Cope <todd@t3-i.com>
2021-03-04 11:27:20 -0600

    Fix function declaration.

--- src/canvas_helpers.h
+++ src/canvas_helpers.h
@@ -5,7 +5,7 @@
 #include "canvas.h"
 
 void quixel_get_canvas_dimensions(QUIXEL_CANVAS * cp, int * offset_x, int * offset_y, int * width, int * height, int flags_filter);
-void quixel_render_canvas_to_bitmap(QUIXEL_CANVAS * cp, int start_layer, int end_layer, int flags_filter, ALLEGRO_BITMAP * bp);
+void quixel_render_canvas_to_bitmap(QUIXEL_CANVAS * cp, int start_layer, int end_layer, int x, int y, int w, int h, int flags_filter, ALLEGRO_BITMAP * bp);
 ALLEGRO_BITMAP * quixel_get_bitmap_from_canvas(QUIXEL_CANVAS * cp, int start_layer, int end_layer, int flags_filter);
 void quixel_import_bitmap_to_canvas(QUIXEL_CANVAS * cp, ALLEGRO_BITMAP * bp, int layer, int x, int y);
 
