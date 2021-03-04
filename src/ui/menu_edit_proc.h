97c1e40da709e540dbd6815ee534aef02215ffc4
Todd Cope <todd@t3-i.com>
2021-03-04 16:35:26 -0600

    Add some placeholders for functions we will be adding.

--- src/ui/menu_edit_proc.h
+++ src/ui/menu_edit_proc.h
@@ -1,6 +1,9 @@
 #ifndef QUIXEL_MENU_EDIT_PROC_H
 #define QUIXEL_MENU_EDIT_PROC_H
 
+int quixel_menu_edit_add_frame(int id, void * data);
+int quixel_menu_edit_frame_selection(int id, void * data);
+int quixel_menu_edit_delete_current_frame(int id, void * data);
 int quixel_menu_edit_delete_layer(int id, void * data);
 
 #endif
