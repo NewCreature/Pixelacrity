136863db84670ceb94f321c413215f589a5498f0
Todd Cope <todd@t3-i.com>
2021-03-04 17:07:52 -0600

    Work on selection tool.

--- src/ui/tool_selection.c
+++ src/ui/tool_selection.c
@@ -5,20 +5,20 @@
 
 void quixel_tool_selection_logic(QUIXEL_CANVAS_EDITOR * cep)
 {
-	ALLEGRO_TRANSFORM identity;
-	ALLEGRO_STATE old_state;
-	float start_x, start_y, end_x, end_y;
+	int start_x, start_y, end_x, end_y;
 
 	start_x = cep->click_x;
 	start_y = cep->click_y;
 	end_x = cep->hover_x;
 	end_y = cep->hover_y;
 
+	quixel_sort_coordinates(&start_x, &end_x);
+	quixel_sort_coordinates(&start_y, &end_y);
 	cep->selection.layer = cep->current_layer;
 	cep->selection.x = start_x;
 	cep->selection.y = start_y;
-	cep->selection.width = end_x - start_x;
-	cep->selection.height = end_y - start_y;
+	cep->selection.width = end_x - start_x + 1;
+	cep->selection.height = end_y - start_y + 1;
 }
 
 void quixel_tool_selection_render(QUIXEL_CANVAS_EDITOR * cep)
