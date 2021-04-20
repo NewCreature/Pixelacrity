82dad78ea2cdc5d045850afddbca4b7ff09564df
Todd Cope <todd@t3-i.com>
2021-04-20 12:02:03 -0500

    Define more undo types.

--- src/ui/undo.h
+++ src/ui/undo.h
@@ -1,7 +1,9 @@
 #ifndef QUIXEL_UNDO_H
 #define QUIXEL_UNDO_H
 
-#define QUIXEL_UNDO_TYPE_TOOL 0
+#define QUIXEL_UNDO_TYPE_TOOL       0
+#define QUIXEL_UNDO_TYPE_FRAME      1
+#define QUIXEL_UNDO_FLOAT_SELECTION 2
 
 bool quixel_make_tool_undo(QUIXEL_CANVAS_EDITOR * cep, int layer, int x, int y, int width, int height, const char * fn);
 bool quixel_make_tool_redo(QUIXEL_CANVAS_EDITOR * cep, int layer, int x, int y, int width, int height, const char * fn);
