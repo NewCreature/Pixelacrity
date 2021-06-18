f1a4f553d923d369483c11d06e70763f90e6062d
Todd Cope <todd@t3-i.com>
2021-06-18 16:14:22 -0500

    Prevent Cut, Copy, and Paste from being used if they should be unavailable.

--- src/ui/menu/menu_edit_proc.c
+++ src/ui/menu/menu_edit_proc.c
@@ -49,40 +49,43 @@ int quixel_menu_edit_cut(int id, void * data)
 	APP_INSTANCE * app = (APP_INSTANCE *)data;
 	char undo_path[1024];
 
-	if(!app->canvas_editor->selection.bitmap)
+	if(app->canvas_editor->selection.box.width > 0 && app->canvas_editor->selection.box.height > 0)
 	{
-		quixel_get_undo_path("undo", app->canvas_editor->undo_count - 1, undo_path, 1024);
-/*		if(quixel_make_tool_undo(app->canvas_editor, "Cut Selection", app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height, undo_path))
+		if(!app->canvas_editor->selection.bitmap)
 		{
-			quixel_finalize_undo(app->canvas_editor);
-			quixel_update_undo_name(app->canvas_editor);
-			quixel_update_redo_name(app->canvas_editor);
-			t3f_refresh_menus();
-		} */
-		pa_copy_canvas_to_clipboard(app->canvas_editor, app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height);
-		quixel_draw_primitive_to_canvas(app->canvas_editor->canvas, app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.end_x, app->canvas_editor->selection.box.end_y, NULL, al_map_rgba_f(0.0, 0.0, 0.0, 0.0), QUIXEL_RENDER_COPY, quixel_draw_filled_rectangle);
-		app->canvas_editor->selection.box.width = 0;
-		app->canvas_editor->selection.box.height = 0;
-	}
-	else
-	{
-		if(pa_copy_bitmap_to_clipboard(app->canvas_editor, app->canvas_editor->selection.bitmap))
-		{
-			if(app->canvas_editor->selection.bitmap)
+			quixel_get_undo_path("undo", app->canvas_editor->undo_count - 1, undo_path, 1024);
+/*			if(quixel_make_tool_undo(app->canvas_editor, "Cut Selection", app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height, undo_path))
 			{
-				al_destroy_bitmap(app->canvas_editor->selection.bitmap);
-				app->canvas_editor->selection.bitmap = NULL;
-			}
-			if(app->canvas_editor->selection.combined_bitmap)
-			{
-				al_destroy_bitmap(app->canvas_editor->selection.combined_bitmap);
-				app->canvas_editor->selection.combined_bitmap = NULL;
-			}
+				quixel_finalize_undo(app->canvas_editor);
+				quixel_update_undo_name(app->canvas_editor);
+				quixel_update_redo_name(app->canvas_editor);
+				t3f_refresh_menus();
+			} */
+			pa_copy_canvas_to_clipboard(app->canvas_editor, app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height);
+			quixel_draw_primitive_to_canvas(app->canvas_editor->canvas, app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.end_x, app->canvas_editor->selection.box.end_y, NULL, al_map_rgba_f(0.0, 0.0, 0.0, 0.0), QUIXEL_RENDER_COPY, quixel_draw_filled_rectangle);
 			app->canvas_editor->selection.box.width = 0;
 			app->canvas_editor->selection.box.height = 0;
 		}
+		else
+		{
+			if(pa_copy_bitmap_to_clipboard(app->canvas_editor, app->canvas_editor->selection.bitmap))
+			{
+				if(app->canvas_editor->selection.bitmap)
+				{
+					al_destroy_bitmap(app->canvas_editor->selection.bitmap);
+					app->canvas_editor->selection.bitmap = NULL;
+				}
+				if(app->canvas_editor->selection.combined_bitmap)
+				{
+					al_destroy_bitmap(app->canvas_editor->selection.combined_bitmap);
+					app->canvas_editor->selection.combined_bitmap = NULL;
+				}
+				app->canvas_editor->selection.box.width = 0;
+				app->canvas_editor->selection.box.height = 0;
+			}
+		}
+		t3f_refresh_menus();
 	}
-	t3f_refresh_menus();
 
 	return 0;
 }
@@ -92,25 +95,28 @@ int quixel_menu_edit_copy(int id, void * data)
 	APP_INSTANCE * app = (APP_INSTANCE *)data;
 	char undo_path[1024];
 
-	if(!app->canvas_editor->selection.bitmap)
+	if(app->canvas_editor->selection.box.width > 0 && app->canvas_editor->selection.box.height > 0)
 	{
-		quixel_get_undo_path("undo", app->canvas_editor->undo_count - 1, undo_path, 1024);
-/*		if(quixel_make_tool_undo(app->canvas_editor, "Cut Selection", app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height, undo_path))
+		if(!app->canvas_editor->selection.bitmap)
 		{
-			quixel_finalize_undo(app->canvas_editor);
-			quixel_update_undo_name(app->canvas_editor);
-			quixel_update_redo_name(app->canvas_editor);
-			t3f_refresh_menus();
-		} */
-		pa_copy_canvas_to_clipboard(app->canvas_editor, app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height);
-	}
-	else
-	{
-		if(pa_copy_bitmap_to_clipboard(app->canvas_editor, app->canvas_editor->selection.bitmap))
-		{
+/*			quixel_get_undo_path("undo", app->canvas_editor->undo_count - 1, undo_path, 1024);
+			if(quixel_make_tool_undo(app->canvas_editor, "Cut Selection", app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height, undo_path))
+			{
+				quixel_finalize_undo(app->canvas_editor);
+				quixel_update_undo_name(app->canvas_editor);
+				quixel_update_redo_name(app->canvas_editor);
+				t3f_refresh_menus();
+			} */
+			pa_copy_canvas_to_clipboard(app->canvas_editor, app->canvas_editor->current_layer, app->canvas_editor->selection.box.start_x, app->canvas_editor->selection.box.start_y, app->canvas_editor->selection.box.width, app->canvas_editor->selection.box.height);
 		}
+		else
+		{
+			if(pa_copy_bitmap_to_clipboard(app->canvas_editor, app->canvas_editor->selection.bitmap))
+			{
+			}
+		}
+		t3f_refresh_menus();
 	}
-	t3f_refresh_menus();
 
 	return 0;
 }
@@ -120,29 +126,32 @@ int quixel_menu_edit_paste(int id, void * data)
 	APP_INSTANCE * app = (APP_INSTANCE *)data;
 	ALLEGRO_STATE old_state;
 
-	al_store_state(&old_state, ALLEGRO_STATE_NEW_BITMAP_PARAMETERS);
-	al_set_new_bitmap_flags(ALLEGRO_NO_PREMULTIPLIED_ALPHA);
-	if(app->canvas_editor->selection.bitmap)
+	if(app->canvas_editor->clipboard.bitmap)
 	{
-		quixel_handle_unfloat_canvas_editor_selection(app->canvas_editor, &app->canvas_editor->selection.box);
-	}
-	app->canvas_editor->selection.bitmap = al_clone_bitmap(app->canvas_editor->clipboard.bitmap);
-	if(app->canvas_editor->selection.bitmap)
-	{
-		app->canvas_editor->selection.combined_bitmap = al_create_bitmap(al_get_bitmap_width(app->canvas_editor->selection.bitmap), al_get_bitmap_height(app->canvas_editor->selection.bitmap));
-		if(app->canvas_editor->selection.combined_bitmap)
+		al_store_state(&old_state, ALLEGRO_STATE_NEW_BITMAP_PARAMETERS);
+		al_set_new_bitmap_flags(ALLEGRO_NO_PREMULTIPLIED_ALPHA);
+		if(app->canvas_editor->selection.bitmap)
 		{
-			quixel_initialize_box(&app->canvas_editor->selection.box, app->canvas_editor->view_x, app->canvas_editor->view_y, al_get_bitmap_width(app->canvas_editor->selection.bitmap), al_get_bitmap_height(app->canvas_editor->selection.bitmap), app->canvas_editor->peg_bitmap);
-			quixel_update_box_handles(&app->canvas_editor->selection.box, app->canvas_editor->view_x, app->canvas_editor->view_y, app->canvas_editor->view_zoom);
+			quixel_handle_unfloat_canvas_editor_selection(app->canvas_editor, &app->canvas_editor->selection.box);
 		}
-		else
+		app->canvas_editor->selection.bitmap = al_clone_bitmap(app->canvas_editor->clipboard.bitmap);
+		if(app->canvas_editor->selection.bitmap)
 		{
-			al_destroy_bitmap(app->canvas_editor->selection.bitmap);
-			app->canvas_editor->selection.bitmap = NULL;
+			app->canvas_editor->selection.combined_bitmap = al_create_bitmap(al_get_bitmap_width(app->canvas_editor->selection.bitmap), al_get_bitmap_height(app->canvas_editor->selection.bitmap));
+			if(app->canvas_editor->selection.combined_bitmap)
+			{
+				quixel_initialize_box(&app->canvas_editor->selection.box, app->canvas_editor->view_x, app->canvas_editor->view_y, al_get_bitmap_width(app->canvas_editor->selection.bitmap), al_get_bitmap_height(app->canvas_editor->selection.bitmap), app->canvas_editor->peg_bitmap);
+				quixel_update_box_handles(&app->canvas_editor->selection.box, app->canvas_editor->view_x, app->canvas_editor->view_y, app->canvas_editor->view_zoom);
+			}
+			else
+			{
+				al_destroy_bitmap(app->canvas_editor->selection.bitmap);
+				app->canvas_editor->selection.bitmap = NULL;
+			}
 		}
+		al_restore_state(&old_state);
+		t3f_refresh_menus();
 	}
-	al_restore_state(&old_state);
-	t3f_refresh_menus();
 
 	return 0;
 }
