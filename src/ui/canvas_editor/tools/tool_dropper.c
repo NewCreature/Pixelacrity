6529fb0cd706456b6c59960a47db75807f413aa9
Todd Cope <todd@t3-i.com>
2021-07-07 14:33:28 -0500

    Fix dropper not picking up color change if the base color matches the color being picked up.

--- src/ui/canvas_editor/tools/tool_dropper.c
+++ src/ui/canvas_editor/tools/tool_dropper.c
@@ -6,9 +6,11 @@ void pa_tool_dropper_logic(PA_CANVAS_EDITOR * cep, int button)
 	if(button == 1)
 	{
 		cep->left_base_color = pa_get_canvas_pixel(cep->canvas, cep->current_layer, cep->hover_x, cep->hover_y);
+		cep->left_color = cep->left_base_color;
 	}
 	else
 	{
 		cep->right_base_color = pa_get_canvas_pixel(cep->canvas, cep->current_layer, cep->hover_x, cep->hover_y);
+		cep->right_color = cep->right_base_color;
 	}
 }
