7b8e20f32d6fe2d84ac5896396b24ec9f7e4c73f
Todd Cope <todd@t3-i.com>
2021-07-28 12:12:43 -0500

    Only create undo state when we are actually going through with deleting the layer.

--- src/ui/menu/menu_layer_proc.c
+++ src/ui/menu/menu_layer_proc.c
@@ -34,16 +34,16 @@ int pa_menu_layer_delete(int id, void * data)
 	char undo_path[1024];
 
 	t3f_debug_message("Enter pa_menu_layer_delte()\n");
-	pa_get_undo_path("undo", app->canvas_editor->undo_count, undo_path, 1024);
-	if(pa_make_remove_layer_undo(app->canvas_editor, app->canvas_editor->current_layer, undo_path))
-	{
-		app->canvas_editor->undo_count++;
-		app->canvas_editor->redo_count = 0;
-		pa_update_undo_name(app->canvas_editor);
-		pa_update_redo_name(app->canvas_editor);
-	}
 	if(app->canvas->layer_max > 1 && app->canvas_editor->current_layer < app->canvas->layer_max)
 	{
+		pa_get_undo_path("undo", app->canvas_editor->undo_count, undo_path, 1024);
+		if(pa_make_remove_layer_undo(app->canvas_editor, app->canvas_editor->current_layer, undo_path))
+		{
+			app->canvas_editor->undo_count++;
+			app->canvas_editor->redo_count = 0;
+			pa_update_undo_name(app->canvas_editor);
+			pa_update_redo_name(app->canvas_editor);
+		}
 		pa_remove_canvas_layer(app->canvas, app->canvas_editor->current_layer);
 		if(app->canvas_editor->current_layer >= app->canvas->layer_max)
 		{
