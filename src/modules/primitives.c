25abc46b5dcd2bef17a52f743b5409d172ca097e
Todd Cope <todd@t3-i.com>
2021-08-18 13:36:50 -0500

    Center brush on cursor position.

--- src/modules/primitives.c
+++ src/modules/primitives.c
@@ -20,19 +20,27 @@ void pa_sort_coordinates(int * x1, int * x2)
 	}
 }
 
-void pa_do_line(int x1, int y1, int x2, int y2, void * dp, void (*proc)(int, int, void *))
+void pa_do_line(int x1, int y1, int x2, int y2, void * dp, void (*proc)(int, int, int, int, void *))
 {
-   int dx = x2-x1;
-   int dy = y2-y1;
-   int i1, i2;
-   int x, y;
-   int dd;
+	BRUSH_DATA * brush_data = (BRUSH_DATA *)dp;
+	int dx = x2-x1;
+	int dy = y2-y1;
+	int i1, i2;
+	int x, y;
+	int dd;
+	int cx = 0, cy = 0;
+
+	if(brush_data->bitmap)
+	{
+		cx = al_get_bitmap_width(brush_data->bitmap) / 2;
+		cy = al_get_bitmap_height(brush_data->bitmap) / 2;
+	}
 
    /* worker macro */
    #define DO_LINE(pri_sign, pri_c, pri_cond, sec_sign, sec_c, sec_cond)     \
    {                                                                         \
       if (d##pri_c == 0) {                                                   \
-	 proc(x1, y1, dp);                                               \
+	 proc(x1, y1, cx, cy, dp);                                               \
 	 return;                                                             \
       }                                                                      \
 									     \
@@ -44,7 +52,7 @@ void pa_do_line(int x1, int y1, int x2, int y2, void * dp, void (*proc)(int, int
       y = y1;                                                                \
 									     \
       while (pri_c pri_cond pri_c##2) {                                      \
-	 proc(x, y, dp);                                                 \
+	 proc(x, y, cx, cy, dp);                                                 \
 									     \
 	 if (dd sec_cond 0) {                                                \
 	    sec_c = sec_c sec_sign 1;                                        \
@@ -105,64 +113,71 @@ void pa_do_line(int x1, int y1, int x2, int y2, void * dp, void (*proc)(int, int
    #undef DO_LINE
 }
 
-void pa_do_ellipse(int ix, int iy, float rx0, float ry0, void * dp, void (*proc)(int, int, void *))
+void pa_do_ellipse(int ix, int iy, float rx0, float ry0, void * dp, void (*proc)(int, int, int, int, void *))
 {
-   int rx = 0, ry = 0;
-   int x, y;
-   float x_change;
-   float y_change;
-   float ellipse_error;
-   float two_a_sq;
-   float two_b_sq;
-   float stopping_x;
-   float stopping_y;
-   int midway_x = 0;
-	 int shift_x = 0;
-	 int shift_y = 0;
+	BRUSH_DATA * brush_data = (BRUSH_DATA *)dp;
+	int rx = 0, ry = 0;
+	int x, y;
+	int cx = 0, cy = 0;
+	float x_change;
+	float y_change;
+	float ellipse_error;
+	float two_a_sq;
+	float two_b_sq;
+	float stopping_x;
+	float stopping_y;
+	int midway_x = 0;
+	int shift_x = 0;
+	int shift_y = 0;
 
-	 if(rx0 > 0.0)
-	 {
-		 rx = rx0;
-	 }
-	 if(ry0 > 0.0)
-	 {
-		 ry = ry0;
-	 }
-	 if(fmod(rx0, rx))
-	 {
-		 shift_x = -1;
-	 }
-	 if(fmod(ry0, ry))
-	 {
-		 shift_y = -1;
-	 }
+	if(brush_data->bitmap)
+	{
+		cx = al_get_bitmap_width(brush_data->bitmap) / 2;
+		cy = al_get_bitmap_height(brush_data->bitmap) / 2;
+	}
+	if(rx0 > 0.0)
+	{
+	 rx = rx0;
+	}
+	if(ry0 > 0.0)
+	{
+	 ry = ry0;
+	}
+	if(fmod(rx0, rx))
+	{
+	 shift_x = -1;
+	}
+	if(fmod(ry0, ry))
+	{
+	 shift_y = -1;
+	}
 
-   two_a_sq = 2 * rx * rx;
-   two_b_sq = 2 * ry * ry;
+	two_a_sq = 2 * rx * rx;
+	two_b_sq = 2 * ry * ry;
 
-   x = rx;
-   y = 0;
+	x = rx;
+	y = 0;
 
-   x_change = ry * ry * (1 - 2 * rx);
-   y_change = rx * rx;
-   ellipse_error = 0.0;
+	x_change = ry * ry * (1 - 2 * rx);
+	y_change = rx * rx;
+	ellipse_error = 0.0;
 
-   /* The following two variables decide when to stop.  It's easier than
-    * solving for this explicitly.
-    */
-   stopping_x = two_b_sq * rx;
-   stopping_y = 0.0;
+	/* The following two variables decide when to stop.  It's easier than
+	 * solving for this explicitly.
+	 */
+	stopping_x = two_b_sq * rx;
+	stopping_y = 0.0;
 
    /* First set of points. */
    while (y <= ry) {
-      proc(ix + x, iy + y, dp);
+      proc(ix + x, iy + y, cx, cy, dp);
       if (x != 0) {
-         proc(ix - x + shift_x, iy + y, dp);
+         proc(ix - x + shift_x, iy + y, cx, cy, dp);
       }
       if (y != 0) {
-         proc(ix + x, iy - y + shift_y, dp);
+         proc(ix + x, iy - y + shift_y, cx, cy, dp);
          if (x != 0) {
-            proc(ix - x + shift_x, iy - y + shift_y, dp);
+            proc(ix - x + shift_x, iy - y + shift_y, cx, cy, dp);
          }
       }
 
@@ -197,14 +212,14 @@ void pa_do_ellipse(int ix, int iy, float rx0, float ry0, void * dp, void (*proc)
    ellipse_error = 0.0;
 
    while (x < midway_x) {
-      proc(ix + x, iy + y, dp);
+      proc(ix + x, iy + y, cx, cy, dp);
       if (x != 0) {
-         proc(ix - x + shift_x, iy + y, dp);
+         proc(ix - x + shift_x, iy + y, cx, cy, dp);
       }
       if (y != 0) {
-         proc(ix + x, iy - y + shift_y, dp);
+         proc(ix + x, iy - y + shift_y, cx, cy, dp);
          if (x != 0) {
-            proc(ix - x + shift_x, iy - y + shift_y, dp);
+            proc(ix - x + shift_x, iy - y + shift_y, cx, cy, dp);
          }
       }
 
@@ -224,21 +239,21 @@ void pa_do_ellipse(int ix, int iy, float rx0, float ry0, void * dp, void (*proc)
 	 /* fill in holes */
 	 if(shift_x)
 	 {
-		 proc(ix + shift_x, iy - ry + shift_y, dp);
-		 proc(ix + shift_x, iy + ry, dp);
+		 proc(ix + shift_x, iy - ry + shift_y, cx, cy, dp);
+		 proc(ix + shift_x, iy + ry, cx, cy, dp);
 	 }
 	 if(shift_y)
 	 {
-		 proc(ix - rx + shift_x, iy + shift_y, dp);
-		 proc(ix + rx, iy + shift_y, dp);
+		 proc(ix - rx + shift_x, iy + shift_y, cx, cy, dp);
+		 proc(ix + rx, iy + shift_y, cx, cy, dp);
 	 }
 }
 
-static void brush_draw(int x, int y, void * dp)
+static void brush_draw(int x, int y, int cx, int cy, void * dp)
 {
 	BRUSH_DATA * brush_data = (BRUSH_DATA *)dp;
 
-	al_draw_tinted_bitmap(brush_data->bitmap, brush_data->color, x, y, 0);
+	al_draw_tinted_bitmap(brush_data->bitmap, brush_data->color, x - cx , y - cy, 0);
 }
 
 void pa_draw_line(int x1, int y1, int x2, int y2, ALLEGRO_BITMAP * bp, ALLEGRO_COLOR color)
