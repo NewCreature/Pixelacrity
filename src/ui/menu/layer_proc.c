cb0f9a6ded5c4de178bdb17370e9a5621b43a2a3
Todd Cope <todd@t3-i.com>
2021-09-16 11:54:32 -0500

    Expand and contract selection and clipboard bitmap stacks when adding/removing layers from canvas.

--- src/ui/menu/layer_proc.c
+++ src/ui/menu/layer_proc.c
@@ -9,6 +9,8 @@ int pa_menu_layer_add(int id, void * data)
 {
 	APP_INSTANCE * app = (APP_INSTANCE *)data;
 	char undo_path[1024];
+	bool expand_selection_stack = false;
+	bool expand_clipboard_stack = false;
 
 	t3f_debug_message("Enter pa_menu_layer_add()\n");
 	pa_get_undo_path("undo", app->canvas_editor->undo_count, undo_path, 1024);
@@ -16,8 +18,26 @@ int pa_menu_layer_add(int id, void * data)
 	{
 		pa_finalize_undo(app->canvas_editor);
 	}
+	if(app->canvas_editor->selection.bitmap_stack && app->canvas_editor->selection.bitmap_stack->layers == app->canvas->layer_max)
+	{
+		expand_selection_stack = true;
+	}
+	if(app->canvas_editor->clipboard.bitmap_stack && app->canvas_editor->clipboard.bitmap_stack->layers == app->canvas->layer_max)
+	{
+		expand_clipboard_stack = true;
+	}
 	if(pa_add_canvas_layer(app->canvas, -1))
 	{
+		if(expand_selection_stack)
+		{
+			pa_add_layer_to_bitmap_stack(app->canvas_editor->selection.bitmap_stack, NULL, -1);
+			app->canvas_editor->selection.layer_max++;
+		}
+		if(expand_clipboard_stack)
+		{
+			pa_add_layer_to_bitmap_stack(app->canvas_editor->clipboard.bitmap_stack, NULL, -1);
+			app->canvas_editor->clipboard.layer_max--;
+		}
 		pa_select_canvas_editor_layer(app->canvas_editor, app->canvas_editor->canvas->layer_max - 1);
 		app->canvas_editor->modified++;
 		pa_set_window_message(NULL);
@@ -31,16 +51,36 @@ int pa_menu_layer_delete(int id, void * data)
 {
 	APP_INSTANCE * app = (APP_INSTANCE *)data;
 	char undo_path[1024];
+	bool contract_selection_stack = false;
+	bool contract_clipboard_stack = false;
 
 	t3f_debug_message("Enter pa_menu_layer_delte()\n");
 	if(app->canvas->layer_max > 1 && app->canvas_editor->current_layer < app->canvas->layer_max)
 	{
+		if(app->canvas_editor->selection.bitmap_stack && app->canvas_editor->selection.bitmap_stack->layers == app->canvas->layer_max)
+		{
+			contract_selection_stack = true;
+		}
+		if(app->canvas_editor->clipboard.bitmap_stack && app->canvas_editor->clipboard.bitmap_stack->layers == app->canvas->layer_max)
+		{
+			contract_clipboard_stack = true;
+		}
 		pa_get_undo_path("undo", app->canvas_editor->undo_count, undo_path, 1024);
 		if(pa_make_remove_layer_undo(app->canvas_editor, app->canvas_editor->current_layer, undo_path))
 		{
 			pa_finalize_undo(app->canvas_editor);
 		}
 		pa_remove_canvas_layer(app->canvas, app->canvas_editor->current_layer);
+		if(contract_selection_stack)
+		{
+			pa_remove_layer_from_bitmap_stack(app->canvas_editor->selection.bitmap_stack, app->canvas_editor->current_layer);
+			app->canvas_editor->selection.layer_max--;
+		}
+		if(contract_clipboard_stack)
+		{
+			pa_remove_layer_from_bitmap_stack(app->canvas_editor->clipboard.bitmap_stack, app->canvas_editor->current_layer);
+			app->canvas_editor->clipboard.layer_max--;
+		}
 		if(app->canvas_editor->current_layer >= app->canvas->layer_max)
 		{
 			pa_select_canvas_editor_layer(app->canvas_editor, app->canvas_editor->canvas->layer_max - 1);
