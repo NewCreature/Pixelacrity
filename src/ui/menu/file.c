8e476b2e507d6261ee9b9e695b6e78beeb49b607
Todd Cope <todd@t3-i.com>
2021-09-29 14:18:13 -0500

    Disable 'File->Export' menu item if the canvas is empty and we have no frames.

--- src/ui/menu/file.c
+++ src/ui/menu/file.c
@@ -1,12 +1,12 @@
 #include "t3f/t3f.h"
 #include "instance.h"
+#include "modules/canvas/canvas_helpers.h"
 #include "menu.h"
 #include "file_proc.h"
 
 static int menu_reexport_update_proc(ALLEGRO_MENU * mp, int item, void * data)
 {
 	APP_INSTANCE * app = (APP_INSTANCE *)data;
-	int i;
 
 	if(app->canvas_editor->export_path || (app->canvas->frame_max > 0 && app->canvas->frame[app->canvas_editor->current_frame]->export_path))
 	{
@@ -39,6 +39,31 @@ static int menu_reexport_all_update_proc(ALLEGRO_MENU * mp, int item, void * dat
 	return 0;
 }
 
+static int menu_export_update_proc(ALLEGRO_MENU * mp, int item, void * data)
+{
+	APP_INSTANCE * app = (APP_INSTANCE *)data;
+	int i;
+	int x, y, w, h;
+
+	if(app->canvas->frame_max > 0)
+	{
+		t3f_set_menu_item_flags(mp, item, 0);
+	}
+	else
+	{
+		pa_get_canvas_dimensions(app->canvas, &x, &y, &w, &h, 0, false);
+		if(w > 0 && h > 0)
+		{
+			t3f_set_menu_item_flags(mp, item, 0);
+		}
+		else
+		{
+			t3f_set_menu_item_flags(mp, item, ALLEGRO_MENU_ITEM_DISABLED);
+		}
+	}
+	return 0;
+}
+
 ALLEGRO_MENU * pa_create_file_menu(void)
 {
 	ALLEGRO_MENU * mp;
@@ -55,7 +80,7 @@ ALLEGRO_MENU * pa_create_file_menu(void)
 	t3f_add_menu_item(mp, NULL, 0, NULL, NULL, NULL);
 	t3f_add_menu_item(mp, "Import", 0, NULL, pa_menu_file_import, pa_menu_base_update_proc);
 	t3f_add_menu_item(mp, NULL, 0, NULL, NULL, NULL);
-	t3f_add_menu_item(mp, "Export", 0, NULL, pa_menu_file_export, pa_menu_base_update_proc);
+	t3f_add_menu_item(mp, "Export", 0, NULL, pa_menu_file_export, menu_export_update_proc);
 	t3f_add_menu_item(mp, "Re-Export", 0, NULL, pa_menu_file_reexport, menu_reexport_update_proc);
 	t3f_add_menu_item(mp, "Re-Export All", 0, NULL, pa_menu_file_reexport_all, menu_reexport_all_update_proc);
 	#ifndef ALLEGRO_MACOSX
