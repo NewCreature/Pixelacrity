2832240bd9a0b4a1fd30bb41371e0b3a9a8e246a
Todd Cope <todd@t3-i.com>
2021-09-29 11:09:11 -0500

    Include color alpha when when determining the new color result from sliding the shade slider.

--- src/ui/gui/canvas_editor/MSG_IDLE.c
+++ src/ui/gui/canvas_editor/MSG_IDLE.c
@@ -66,7 +66,7 @@ static void update_color_selections(PA_CANVAS_EDITOR * canvas_editor)
 	if(canvas_editor->right_color.old_shade_slider_d2 != canvas_editor->right_color.shade_slider_element->d2)
 	{
 		canvas_editor->right_color.shade_color = pa_shade_color(canvas_editor->right_color.base_color, (float)canvas_editor->right_color.shade_slider_element->d2 / 1000.0);
-		canvas_editor->right_color.color = canvas_editor->right_color.shade_color;
+		canvas_editor->right_color.color = pa_alpha_color(canvas_editor->right_color.shade_color, pa_get_color_alpha(canvas_editor->right_color.color));
 		canvas_editor->right_color.last_shade_color = canvas_editor->right_color.shade_color;
 	}
 
