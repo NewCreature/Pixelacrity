032d5b9f8c381c30d7764e05667deabab3055ba6
Todd Cope <todd@t3-i.com>
2021-11-15 12:02:00 -0600

    Begin implementing canvas area flood fill.

--- src/modules/canvas/flood_fill.c
+++ src/modules/canvas/flood_fill.c
@@ -19,9 +19,33 @@ static ALLEGRO_COLOR get_pixel(PA_CANVAS * cp, int layer, int x, int y, ALLEGRO_
 	return al_get_pixel(*current_bp, *ox, *oy);
 }
 
+static ALLEGRO_COLOR get_pixel_2(PA_CANVAS * cp, int layer, int x, int y, int * ox, int * oy)
+{
+	ALLEGRO_BITMAP * current_bp = cp->layer[layer]->bitmap[y / cp->bitmap_size][x / cp->bitmap_size];
+	*ox = x % cp->bitmap_size;
+	*oy = y % cp->bitmap_size;
+
+	if(current_bp)
+	{
+		if(!al_is_bitmap_locked(current_bp))
+		{
+			al_lock_bitmap(current_bp, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READWRITE);
+		}
+		return al_get_pixel(current_bp, *ox, *oy);
+	}
+	return al_map_rgba_f(0.0, 0.0, 0.0, 0.0);
+}
+
 static void put_pixel(PA_CANVAS * cp, int layer, int ox, int oy, ALLEGRO_COLOR color, ALLEGRO_BITMAP * current_bp)
 {
 	al_set_target_bitmap(current_bp);
+	al_put_pixel(ox, oy, color);
+}
+
+static void put_pixel_2(PA_CANVAS * cp, int layer, int ox, int oy, ALLEGRO_COLOR color)
+{
+	pa_expand_canvas(cp, layer, ox, oy);
+	al_set_target_bitmap(cp->layer[layer]->bitmap[oy / cp->bitmap_size][ox / cp->bitmap_size]);
 	al_put_pixel(ox, oy, color);
 }
 
@@ -158,6 +182,90 @@ bool pa_flood_fill_canvas(PA_CANVAS * cp, int layer, int start_x, int start_y, A
 		pa_flood_fill_canvas_from_queue(cp, layer, old_color, out_qp);
 	}
 	t3f_debug_message("Exit pa_flood_fill_canvas()\n");
+	return ret;
+}
+
+bool pa_flood_fill_canvas_area(PA_CANVAS * cp, int layer, int left, int top, int right, int bottom, int start_x, int start_y, ALLEGRO_COLOR color, PA_QUEUE * out_qp)
+{
+	PA_QUEUE * qp;
+	ALLEGRO_COLOR old_color;
+	ALLEGRO_COLOR current_color;
+	ALLEGRO_STATE old_state;
+	int x, y, ox, oy;
+	bool ret = true;
+	int shift_x, shift_y;
+
+	t3f_debug_message("Enter pa_flood_fill_canvas_area()\n");
+	if(layer >= cp->layer_max)
+	{
+		t3f_debug_message("Fail pa_flood_fill_canvas_area() 1\n");
+		return false;
+	}
+	al_store_state(&old_state, ALLEGRO_STATE_TARGET_BITMAP);
+	qp = pa_create_queue();
+	if(qp)
+	{
+		old_color = get_pixel_2(cp, layer, start_x, start_y, &ox, &oy);
+		if(pa_color_equal(old_color, color))
+		{
+			ret = true;
+			goto cleanup;
+		}
+		pa_handle_canvas_expansion(cp, left, top, right, bottom, &shift_x, &shift_y);
+		left += shift_x;
+		top += shift_y;
+		right += shift_x;
+		bottom += shift_y;
+		start_x += shift_x;
+		start_y += shift_y;
+		put_pixel_2(cp, layer, ox, oy, color);
+		pa_queue_push(qp, start_x, start_y);
+		pa_queue_push(out_qp, start_x, start_y);
+		while(pa_queue_pop(qp, &x, &y))
+		{
+			current_color = get_pixel_2(cp, layer, x - 1, y, &ox, &oy);
+			if(pa_color_equal(current_color, old_color))
+			{
+				put_pixel_2(cp, layer, ox, oy, color);
+				pa_queue_push(qp, x - 1, y);
+				pa_queue_push(out_qp, x - 1, y);
+			}
+			current_color = get_pixel_2(cp, layer, x + 1, y, &ox, &oy);
+			if(pa_color_equal(current_color, old_color))
+			{
+				put_pixel_2(cp, layer, ox, oy, color);
+				pa_queue_push(qp, x + 1, y);
+				pa_queue_push(out_qp, x + 1, y);
+			}
+			current_color = get_pixel_2(cp, layer, x, y - 1, &ox, &oy);
+			if(pa_color_equal(current_color, old_color))
+			{
+				put_pixel_2(cp, layer, ox, oy, color);
+				pa_queue_push(qp, x, y - 1);
+				pa_queue_push(out_qp, x, y - 1);
+			}
+			current_color = get_pixel_2(cp, layer, x, y + 1, &ox, &oy);
+			if(pa_color_equal(current_color, old_color))
+			{
+				put_pixel_2(cp, layer, ox, oy, color);
+				pa_queue_push(qp, x, y + 1);
+				pa_queue_push(out_qp, x, y + 1);
+			}
+		}
+		pa_destroy_queue(qp);
+	}
+	cleanup:
+	{
+		t3f_debug_message("Cleanup pa_flood_fill_canvas_area()\n");
+		unlock_canvas_layer(cp, layer);
+		al_restore_state(&old_state);
+	}
+	if(!ret)
+	{
+		t3f_debug_message("Revert pa_flood_fill_canvas_area()\n");
+		pa_flood_fill_canvas_from_queue(cp, layer, old_color, out_qp);
+	}
+	t3f_debug_message("Exit pa_flood_fill_canvas_area()\n");
 	return ret;
 }
 
