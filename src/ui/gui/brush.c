72b8f2dd21f8624467e69eb7f1096d6becf45bf6
Todd Cope <todd@t3-i.com>
2022-03-02 12:27:30 -0600

    Improve brush preview.

--- src/ui/gui/brush.c
+++ src/ui/gui/brush.c
@@ -13,6 +13,10 @@ int pa_gui_brush_proc(int msg, T3GUI_ELEMENT * d, int c)
 	float usable_height = d->h - d->theme->state[0].top_margin - d->theme->state[0].bottom_margin;
 	color = d->dp2 ? *(ALLEGRO_COLOR *)d->dp2 : t3f_color_black;
 	float cx, cy;
+	int grid_scale;
+	int grid_offset;
+	int brush_size;
+	int i, j;
 
 	switch(msg)
 	{
@@ -23,18 +27,54 @@ int pa_gui_brush_proc(int msg, T3GUI_ELEMENT * d, int c)
 		}
 		case MSG_DRAW:
 		{
+			brush_size = al_get_bitmap_width(bp);
+			if(al_get_bitmap_height(bp) > brush_size)
+			{
+				brush_size = al_get_bitmap_height(bp);
+			}
+			grid_scale = usable_width / (brush_size + 2);
 			al_draw_filled_rectangle(d->x, d->y, d->x + d->w - 1, d->y + d->h - 1, t3f_color_white);
-			if(al_get_bitmap_width(bp) > usable_width)
+			for(i = 0; i < usable_width / grid_scale; i++)
 			{
-				scale = usable_width / (float)al_get_bitmap_width(bp);
+				al_draw_line(d->x + i * grid_scale + grid_offset, d->y, d->x + i * grid_scale + grid_offset, d->y + d->h, t3f_color_black, 1.0);
 			}
-			if(al_get_bitmap_height(bp) * scale > usable_height)
+			cx = d->x + d->w / 2 - (al_get_bitmap_width(bp) * grid_scale) / 2;
+			cy = d->y + d->h / 2 - (al_get_bitmap_height(bp) * grid_scale) / 2;
+			if(grid_scale > 1)
 			{
-				scale = usable_height / (float)al_get_bitmap_height(bp);
+				for(i = cx; i >= d->x; i -= grid_scale)
+				{
+					al_draw_line(i, d->y, i, d->y + d->h, t3f_color_black, 1.0);
+				}
+				for(i = cx + grid_scale; i <= d->x + d->w; i += grid_scale)
+				{
+					al_draw_line(i, d->y, i, d->y + d->h, t3f_color_black, 1.0);
+				}
+				for(i = cy; i >= d->y; i -= grid_scale)
+				{
+					al_draw_line(d->x, i, d->x + d->w, i, t3f_color_black, 1.0);
+				}
+				for(i = cy + grid_scale; i <= d->y + d->h; i += grid_scale)
+				{
+					al_draw_line(d->x, i, d->x + d->w, i, t3f_color_black, 1.0);
+				}
 			}
-			cx = d->x + d->w / 2 - (al_get_bitmap_width(bp) * scale) / 2;
-			cy = d->y + d->h / 2 - (al_get_bitmap_height(bp) * scale) / 2;
-			al_draw_tinted_scaled_bitmap(bp, color, 0, 0, al_get_bitmap_width(bp), al_get_bitmap_height(bp), cx, cy, al_get_bitmap_width(bp) * scale, al_get_bitmap_height(bp) * scale, 0);
+			if(grid_scale)
+			{
+				al_draw_tinted_scaled_bitmap(bp, color, 0, 0, al_get_bitmap_width(bp), al_get_bitmap_height(bp), cx, cy, al_get_bitmap_width(bp) * grid_scale, al_get_bitmap_height(bp) * grid_scale, 0);
+			}
+			else
+			{
+				if(al_get_bitmap_width(bp) > usable_width)
+				{
+					scale = usable_width / (float)al_get_bitmap_width(bp);
+				}
+				if(al_get_bitmap_height(bp) * scale > usable_height)
+				{
+					scale = usable_height / (float)al_get_bitmap_height(bp);
+				}
+				al_draw_tinted_scaled_bitmap(bp, color, 0, 0, al_get_bitmap_width(bp), al_get_bitmap_height(bp), cx, cy, al_get_bitmap_width(bp) * scale, al_get_bitmap_height(bp) * scale, 0);
+			}
 			break;
 		}
 	}
