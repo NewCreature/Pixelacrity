e9d92271f1e3a8fcf2e93d0f7d4714c804723014
Todd Cope <todd@t3-i.com>
2022-05-10 20:56:53 -0500

    Update to the latest version of T3GUI.

--- src/ui/element_proc/layer_list_proc.h
+++ src/ui/element_proc/layer_list_proc.h
@@ -1,6 +1,6 @@
 #ifndef PA_UI_LAYER_LIST_PROC_H
 #define PA_UI_LAYER_LIST_PROC_H
 
-const char * pa_layer_list_proc(int index, int *num_elem, void *dp3);
+const char * pa_layer_list_proc(int index, int *num_elem, bool * multi, void *dp3);
 
 #endif
