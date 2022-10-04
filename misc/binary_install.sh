6932e9215461d2a01b3b6875600bf59e58937ca5
Todd Cope <todd@t3-i.com>
2022-10-04 17:13:31 -0500

    Don't proceed with installation if executable binary is missing.

--- misc/binary_install.sh
+++ misc/binary_install.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 
-if (( $EUID != 0 )); then
-    echo "This script requires root priveleges."
-    exit 0
-fi
-
 prefix=${PREFIX=/usr/local}
 app_title="T3F_APP_TITLE"
 app_name=T3F_APP_NAME
 app_dir=T3F_APP_DIR
 app_categories=T3F_APP_CATEGORIES
 
-mkdir -p $prefix/share/$app_name
-mkdir -p $prefix/share/doc
-mkdir -p $prefix/share/doc/$app_name
-mkdir -p $prefix/share/icons
-mkdir -p $prefix/share/applications
-mkdir -p $prefix/share/menu
-cp -a bin/data $prefix/share/$app_name
-cp docs/changelog $prefix/share/doc/$app_name/changelog
-cp docs/README $prefix/share/doc/$app_name/README
-cp docs/copyright $prefix/share/doc/$app_name/copyright
-cp icons/icon.svg $prefix/share/icons/$app_name.svg
-cp bin/$app_name $prefix/$app_dir/$app_name
-printf "[Desktop Entry]\nName="$app_title"\nExec=$prefix/$app_dir/$app_name\nIcon=$app_name\nTerminal=false\nType=Application\nCategories=$app_categories;" > $prefix/share/applications/$app_name.desktop
-chmod 755 $prefix/$app_dir/$app_name
-find $prefix/share/$app_name -type f -exec chmod 644 {} \;
-chmod 644 $prefix/share/doc/$app_name/README
-chmod 644 $prefix/share/doc/$app_name/changelog
-chmod 644 $prefix/share/doc/$app_name/copyright
-chmod 644 $prefix/share/icons/$app_name.svg
-chmod 644 $prefix/share/applications/$app_name.desktop
-update-desktop-database
+if [ $EUID != 0 ]
+then
+  echo "Installation of this application requires superuser priveleges. If you are not "
+  echo "already running this script as a superuser you may be asked to authenticate."
+  echo ""
+  sudo $0
+else
+  if which bin/$app_name &>/dev/null
+  then
+    echo "Installing T3F_APP_TITLE for all users..."
+    mkdir -p $prefix/share/$app_name
+    mkdir -p $prefix/share/doc
+    mkdir -p $prefix/share/doc/$app_name
+    mkdir -p $prefix/share/icons
+    mkdir -p $prefix/share/applications
+    mkdir -p $prefix/share/menu
+    cp -a bin/data $prefix/share/$app_name
+    cp docs/changelog $prefix/share/doc/$app_name/changelog
+    cp docs/README $prefix/share/doc/$app_name/README
+    cp docs/copyright $prefix/share/doc/$app_name/copyright
+    cp icons/icon.svg $prefix/share/icons/$app_name.svg
+    cp bin/$app_name $prefix/$app_dir/$app_name
+    printf "[Desktop Entry]\nName="$app_title"\nExec=$prefix/$app_dir/$app_name\nIcon=$app_name\nTerminal=false\nType=Application\nCategories=$app_categories;" > $prefix/share/applications/$app_name.desktop
+    chmod 755 $prefix/$app_dir/$app_name
+    find $prefix/share/$app_name -type f -exec chmod 644 {} \;
+    chmod 644 $prefix/share/doc/$app_name/README
+    chmod 644 $prefix/share/doc/$app_name/changelog
+    chmod 644 $prefix/share/doc/$app_name/copyright
+    chmod 644 $prefix/share/icons/$app_name.svg
+    chmod 644 $prefix/share/applications/$app_name.desktop
+    update-desktop-database
+  else
+    echo "Application executable binary missing. Installation cancelled."
+    echo ""
+  fi
+fi
